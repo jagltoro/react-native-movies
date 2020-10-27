3507bc751becb078f7d55421157b4be49e2865dc
Jesus Gonzalez <jagltoro@gmail.com>
2020-10-26 21:38:22 -0300

    Added Theme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import {ThemeProvider} from '@shopify/restyle';
+
+import {theme} from "./src/Helpers/Theme";
 import {IntroNavigator} from "./src/Intro";
 import {LoadAssets} from "./src/Helpers";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 
 // const assets:any = []
 const fonts = {
+  "Dosis-Light": require("./assets/fonts/Dosis-Light.otf"),
   "Dosis-Regular": require("./assets/fonts/Dosis-Regular.otf"),
+  "Dosis-Bold": require("./assets/fonts/Dosis-Bold.otf"),
   "Dosis-SemiBold": require("./assets/fonts/Dosis-SemiBold.otf"),
   "Nunito-Bold": require("./assets/fonts/Nunito-Bold.otf"),
   "Nunito-Regular": require("./assets/fonts/Nunito-Regular.otf"),
@@ -19,11 +24,12 @@ const fonts = {
 
 export default function App() {
   return (
-    <LoadAssets {...{fonts}}>
-      <SafeAreaProvider>
-          <IntroNavigator/>
-      </SafeAreaProvider>
-    </LoadAssets>
-
+    <ThemeProvider {...{theme}}>
+      <LoadAssets {...{fonts}}>
+        <SafeAreaProvider>
+            <IntroNavigator/>
+        </SafeAreaProvider>
+      </LoadAssets>
+    </ThemeProvider>
   );
 }
